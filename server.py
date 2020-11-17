1f01f6ce82eb48193e8e20933dc90d08e04c2beb
jgabari <j.gabari.2017@alumnos.urjc.es>
2020-11-17 19:56:39 +0100

    ej 9 y 10 json

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+import json
+import time
 
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
@@ -12,23 +14,75 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     Echo server class
     """
     diccionario = {}
+    format = '%Y-%m-%d %H:%M:%S'
 
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
-        print('Cliente en IP: ' + str(self.client_address[0]) + ', puerto: ' + str(self.client_address[1]))
-        print('El cliente nos manda:')
+
+        # imports the dictionary from a json file if it exists
+        self.json2registered()
+
+        # look for expired clients
+        self.expiration()
+
+        print('Cliente en IP:' + str(self.client_address[0]) +
+              ', puerto:' + str(self.client_address[1]) +
+              ' manda:\r')
+        print(self.rfile.read().decode('utf-8'))
         for line in self.rfile:
-            print(line.decode('utf-8'))
-            if line.decode('utf-8').split(' ')[0] == 'REGISTER':
-                client = line.decode('utf-8').split(' ')[1].split(':')[1]
-                self.diccionario[client] = self.client_address
-            elif line.decode('utf-8').split(' ')[0] == 'Expires:':
-                if line.decode('utf-8').split(' ')[1].split('\r')[0] == str(0):
+            word_list = line.decode('utf-8').split()
+            if word_list[0] == 'REGISTER':
+                client = word_list[1].split(':')[1]
+                address = str(self.client_address[0])
+                self.diccionario[client] = {'address': address}
+                self.register2json()
+                # if the client already exists it is updated else it is added
+                print('Añadido ' + client + 'al diccionario.\r')
+            elif word_list[0] == 'Expires:':
+                expires_value = int(word_list[1].split('\r')[0])
+                if expires_value == 0:
                     del self.diccionario[client]
+                    print('Eliminado ' + client + 'por expiración.\r')
+                    self.register2json()
+                else:
+                    expire_time = time.gmtime(time.time() + expires_value)
+                    expires = time.strftime(self.format, expire_time)
+                    self.diccionario[client]['expires'] = expires
+                    self.register2json()
         self.wfile.write(b"SIP/2.0 200 OK")
+
+    def expiration(self):
+        """
+        Look for and delete expired clients
+        """
+        for client in self.diccionario:
+            c_time = time.strftime(self.format, time.gmtime(time.time()))
+            if self.diccionario[client]['expires'] <= c_time:
+                del self.diccionario[client]
+                print('Eliminado ' + client + 'por expiración.\r')
+                self.register2json()
+
+    def register2json(self):
+        """
+        Saves the dictionary in a json file
+        """
+        with open('registered.json', 'w') as jsonfile:
+            json.dump(self.diccionario, jsonfile, indent=4)
+
+    def json2registered(self):
+        """
+        Import the dictionary from a json file if it exists
+        """
+        try:
+            with open('registered.json', 'r') as jsonfile:
+                self.diccionario = json.load(jsonfile)
+        except FileNotFoundError:
+            self.diccionario = self.diccionario
+
+
 
 
 if __name__ == "__main__":
